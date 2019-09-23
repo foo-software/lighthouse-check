d70b664f34aa865b0797e003d6a7cf305b4b7133
Adam Henson <adamhenson1979@gmail.com>
2019-09-22 23:44:06 -0400

    feat: local lighthouse.

--- src/lighthouseCheck.js
+++ src/lighthouseCheck.js
@@ -1,12 +1,22 @@
 import fetchAndWaitForLighthouseAudits from './fetchAndWaitForLighthouseAudits';
 import LighthouseCheckError from './LighthouseCheckError';
 import triggerLighthouse from './triggerLighthouse';
+import localLighthouse from './localLighthouse';
 import { NAME, SUCCESS_CODE_GENERIC } from './constants';
 import { ERROR_NO_RESULTS } from './errorCodes';
 
 export default ({
   apiToken,
+  awsAccessKeyId,
+  awsBucket,
+  awsRegion,
+  awsSecretAccessKey,
+  emulatedFormFactor,
+  locale,
+  outputDirectory,
   tag,
+  throttling,
+  throttlingMethod,
   timeout,
   urls,
   verbose = true,
@@ -14,64 +24,100 @@ export default ({
 }) =>
   new Promise(async (resolve, reject) => {
     try {
-      const triggerResult = await triggerLighthouse({
-        apiToken,
-        tag,
-        timeout,
-        urls,
-        verbose
-      });
+      // we either get the result from the API or directly from
+      // running a lighthouse audit locally.
+      const isLocalAudit = !apiToken;
 
-      if (triggerResult.error) {
-        reject(triggerResult.error);
-        return;
-      }
+      // if we're auditing through the lighthouse-check.com API,
+      // otherwise we're using Lighthouse directly, locally
+      if (!isLocalAudit) {
+        const triggerResult = await triggerLighthouse({
+          apiToken,
+          tag,
+          timeout,
+          urls,
+          verbose
+        });
 
-      // if the user understandably doesn't want to wait for results, return right away
-      if (!wait) {
-        resolve(triggerResult);
-        return;
-      }
+        if (triggerResult.error) {
+          reject(triggerResult.error);
+          return;
+        }
 
-      // if this condition doesn't pass - we got a problem
-      if (triggerResult.data) {
-        // assemble an array of queueIds
-        const queueIds = triggerResult.data.reduce(
-          (accumulator, current) => [
-            ...accumulator,
-            ...(!current.id ? [] : [current.id])
-          ],
-          []
-        );
+        // if the user understandably doesn't want to wait for results, return right away
+        if (!wait) {
+          resolve(triggerResult);
+          return;
+        }
 
         // if this condition doesn't pass - we got a problem
-        if (queueIds.length) {
-          const auditResults = await fetchAndWaitForLighthouseAudits({
-            apiToken,
-            queueIds,
-            timeout,
-            verbose
-          });
+        if (triggerResult.data) {
+          // assemble an array of queueIds
+          const queueIds = triggerResult.data.reduce(
+            (accumulator, current) => [
+              ...accumulator,
+              ...(!current.id ? [] : [current.id])
+            ],
+            []
+          );
 
+          // if this condition doesn't pass - we got a problem
+          if (queueIds.length) {
+            const auditResults = await fetchAndWaitForLighthouseAudits({
+              apiToken,
+              queueIds,
+              timeout,
+              verbose
+            });
+
+            // success
+            resolve({
+              code: SUCCESS_CODE_GENERIC,
+              data: auditResults
+            });
+            return;
+          }
+        }
+
+        const errorMessage = 'Failed to retrieve results.';
+        if (verbose) {
+          console.log(`${NAME}:`, errorMessage);
+        }
+
+        reject(
+          new LighthouseCheckError(errorMessage, {
+            code: ERROR_NO_RESULTS
+          })
+        );
+      } else {
+        const lighthouseAudits = await localLighthouse({
+          awsAccessKeyId,
+          awsBucket,
+          awsRegion,
+          awsSecretAccessKey,
+          emulatedFormFactor,
+          locale,
+          outputDirectory,
+          throttling,
+          throttlingMethod,
+          urls,
+          verbose
+        });
+
+        if (!lighthouseAudits.length) {
+          reject(
+            new LighthouseCheckError('Something went wrong - no results.', {
+              code: ERROR_NO_RESULTS
+            })
+          );
+        } else {
           // success
           resolve({
             code: SUCCESS_CODE_GENERIC,
-            data: auditResults
+            data: lighthouseAudits
           });
-          return;
         }
       }
-
-      const errorMessage = 'Failed to retrieve results.';
-      if (verbose) {
-        console.log(`${NAME}:`, errorMessage);
-      }
-
-      reject(
-        new LighthouseCheckError(errorMessage, {
-          code: ERROR_NO_RESULTS
-        })
-      );
     } catch (error) {
       if (verbose) {
         console.log(`${NAME}:\n`, error);
