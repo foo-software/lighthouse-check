40b4f8e4d63de4c0b135996d29c32bdfbf163f1b
adamhenson <ahenson@paperlesspost.com>
2019-09-24 10:12:01 -0400

    feat: log in module vs CLI only.

---
+++ src/logResults.js
@@ -0,0 +1,45 @@
+import { table, getBorderCharacters } from 'table';
+import getLighthouseAuditTitlesByKey from './helpers/getLighthouseAuditTitlesByKey';
+
+// config for the `table` module (for console logging a table)
+const tableConfig = {
+  border: getBorderCharacters('ramac')
+};
+
+export default ({ result }) => {
+  // header
+  const headerTable = [['Lighthouse Audit']];
+  const headerTableConfig = {
+    ...tableConfig,
+    columns: {
+      0: {
+        paddingLeft: 29,
+        paddingRight: 29
+      }
+    }
+  };
+
+  // log the header
+  console.log(table(headerTable, headerTableConfig));
+
+  // log results
+  result.data.forEach(result => {
+    console.log(`URL: ${result.url}`);
+
+    if (result.report) {
+      console.log(`Report: ${result.report}`);
+    }
+
+    if (result.localReport) {
+      console.log(`Local Report: ${result.localReport}`);
+    }
+
+    const tableData = [
+      getLighthouseAuditTitlesByKey(Object.keys(result.scores)),
+      Object.values(result.scores)
+    ];
+    console.log('\n');
+    console.log(table(tableData, tableConfig));
+    console.log('\n');
+  });
+};
