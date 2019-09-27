a4e7bb463f32f62ed482d5874f0c67db6a434562
adamhenson <ahenson@paperlesspost.com>
2019-09-27 00:16:42 -0400

    Revert "fix: lighthouse score colors."
    This reverts commit d6c381326bc0040f898dde20fd88ea7134201b6f.

--- src/slackNotify.js
+++ src/slackNotify.js
@@ -1,5 +1,4 @@
 import { IncomingWebhook } from '@slack/webhook';
-import getLighthouseScoreColor from './helpers/getLighthouseScoreColor';
 import lighthouseAuditTitles from './lighthouseAuditTitles';
 import { NAME } from './constants';
 
@@ -20,15 +19,23 @@ export default async ({
       typeof prParam !== 'string' || prParam === 'true' ? undefined : prParam;
 
     for (const result of results) {
+      // get the average of all socres
+      const values = Object.values(result.scores);
+      const sum = values.reduce(
+        (accumulator, current) => current + accumulator,
+        0
+      );
+      const average = Math.floor(sum / values.length);
+
       // link the report if we have it
-      let title = !result.report
+      let text = !result.report
         ? 'Lighthouse Audit.'
         : `<${result.report}|Lighthouse Audit>.`;
 
       // if we have a branch
       if (branch) {
         const branchText = !pr ? branch : `<${pr}|${branch}>`;
-        title = `${title} Change made in \`${branchText}\`.`;
+        text = `${text} Change made in \`${branchText}\`.`;
       }
 
       let footer;
@@ -41,17 +48,32 @@ export default async ({
       }
 
       await webhook.send({
-        title,
         text: result.url,
-        attachments: Object.keys(result.scores).map(current => ({
-          color: getLighthouseScoreColor(result.scores[current]),
-          text: `**${lighthouseAuditTitles[current]}**: ${result.scores[current]}`
-        })),
-        ...(!footer
-          ? {}
-          : {
-              footer
-            })
+        attachments: [
+          {
+            color: '#2091fa',
+            fields: [
+              {
+                title: 'Average',
+                value: average,
+                short: true
+              },
+              ...Object.keys(result.scores).map(current => ({
+                title: lighthouseAuditTitles[current],
+                value: result.scores[current],
+                short: true
+              }))
+            ],
+            text,
+            thumb_url:
+              'https://s3.amazonaws.com/foo.software/images/logos/lighthouse.png',
+            ...(!footer
+              ? {}
+              : {
+                  footer
+                })
+          }
+        ]
       });
     }
   } catch (error) {
