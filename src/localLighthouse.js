97a325db06ab554e09ca0aae4f63f1cb4a3cfc2e
Adam Henson <adamhenson1979@gmail.com>
2019-09-28 15:10:23 -0400

    chore: log

--- src/localLighthouse.js
+++ src/localLighthouse.js
@@ -112,6 +112,20 @@ export default async ({
   const auditResults = [];
   let index = 1;
 
+  console.log('params', {
+    awsAccessKeyId,
+    awsBucket,
+    awsRegion,
+    awsSecretAccessKey,
+    emulatedFormFactor,
+    locale,
+    outputDirectory,
+    throttling,
+    throttlingMethod,
+    urls,
+    verbose
+  });
+
   for (const url of urls) {
     if (verbose) {
       console.log(`${NAME}: Auditing (${index}/${urls.length}) ${url}`);
