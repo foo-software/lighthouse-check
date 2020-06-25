92448a0d391f1b803e10e0c57dbfd34d4315fab8
Adam Henson <adamhenson1979@gmail.com>
2020-06-25 12:32:51 -0400

    test: lighthouse check action

---
+++ src/bin/githubAction.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import core from '@actions/core';
+import get from 'lodash.get';
+import github from '@actions/github';
+import lighthouseCheck from './dist/lighthouseCheck';
+
+(async () => {
+  try {
+    const prApiUrl = get(github, 'context.payload.pull_request.url');
+
+    const results = await lighthouseCheck({
+      prCommentAccessToken: process.env.prCommentAccessToken,
+      prCommentEnabled: true,
+      prCommentUrl: !prApiUrl ? undefined : `${prApiUrl}/reviews`,
+      urls: 'https://www.foo.software',
+      verbose: true
+    });
+
+    // yikesers - only strings :(
+    // https://help.github.com/en/articles/contexts-and-expression-syntax-for-github-actions#steps-context
+    core.setOutput('lighthouseCheckResults', JSON.stringify(results));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
