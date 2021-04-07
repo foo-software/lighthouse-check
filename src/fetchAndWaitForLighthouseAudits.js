3ae97a145cba958280092533d853f264d82daaf9
adamhenson <adamhenson1979@gmail.com>
2021-04-07 19:51:42 -0400

    test: debug

--- src/fetchAndWaitForLighthouseAudits.js
+++ src/fetchAndWaitForLighthouseAudits.js
@@ -63,6 +63,7 @@ export default ({
           reject(result.error);
           return;
         } else if (areResultsExpected) {
+          console.log('api results', result.data);
           const audits = result.data.map(current => ({
             name: current.name,
             report: current.report,
