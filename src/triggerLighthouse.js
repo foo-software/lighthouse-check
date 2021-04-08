21ffa485e7817db52f4d852d58d36d69bdda3386
adamhenson <adamhenson1979@gmail.com>
2021-04-07 21:19:17 -0400

    fix: ensure lighthouse comments do not overwrite each other

--- src/triggerLighthouse.js
+++ src/triggerLighthouse.js
@@ -84,7 +84,7 @@ export default async ({
     } else if (isOrb) {
       source = SOURCE_CIRCLECI_ORB;
     }
-
+    console.log('isGitHubAction', isGitHubAction);
     console.log('source', source);
 
     // enqueue urls for Lighthouse audits
