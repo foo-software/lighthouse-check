7724dfcfb9d55da1e885f8ddb38a5fe6e6e3ad78
adamhenson <adamhenson1979@gmail.com>
2022-11-23 07:24:23 -0500

    chore: dependencies

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,14 @@
 // this should only be defined when working locally.
 if (process.env.API_URL) {
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 }
 
-// https://stackoverflow.com/a/69331469
+// a bit of a hack until we only support ES Modules.
+// at least this is the officially recommended hack
+// https://github.com/node-fetch/node-fetch#commonjs
+// note the key here:
+// https://github.com/microsoft/TypeScript/pull/44501#issue-914346744
 const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+  import('node-fetch').then(({ default: module }) => module(...args));
 
 export default fetch;
